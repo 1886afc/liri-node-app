b0505edfec32e72069ba8b9b5c417c36d3e50621
Israel Ponce <Israel Ponce>
2017-03-30 13:10:44 -0400

    getting started

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: '	vGnalooZUdLIr5DW1euPTJqr1',
+  consumer_secret: '	V2d9KeDq4vBs9XSeTaHsoxm4Szz5paq4OJchw1irCcOFLdIuAP',
+  access_token_key: '	274765598-NX1Iz12LZDtSX4b4ukyj93xvrKP37tJwp7PosoV4',
+  access_token_secret: 'VF2KpU38OOV9p3cLEHHHmzjeXoyuMUkwRMO93PU0PjwQx',
+}
